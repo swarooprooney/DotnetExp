fcf171b9d3afd426c8a47613bfde3c259c5bf22f
SWAROOP Shivanna <swarooprooney@gmail.com>
2021-07-12 19:21:08 +0530

    Correcting the folder structure

---
+++ BenchmarkConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<StringManipulationBenchmarks>();
+
+            Console.ReadLine();
+        }
+    }
+}
