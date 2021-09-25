ad4a5223094026df9953d9a1eded9bc141139281
swarooprooney <swarooprooney@gmail.com>
2021-09-25 21:15:45 +0530

    Refit (#6)
    * Creating a Contract project
    
    * Adding refit to create a SDK

---
+++ Tweetbook.Sdk/IIndentityApi.cs
@@ -0,0 +1,19 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1.Request;
+using TweetBook.Contracts.V1.Response;
+
+namespace Tweetbook.Sdk
+{
+    public interface IIndentityApi
+    {
+        [Post("/api/v1/identity/register")]
+        Task<ApiResponse<AuthSuccessResponse>> RegisterAsync([Body] UserRegistrationRequest userRegistrationRequest);
+
+        [Post("/api/v1/identity/login")]
+        Task<ApiResponse<AuthSuccessResponse>> LoginAsync([Body] UserLoginRequest userLoginRequest);
+
+        [Post("/api/v1/identity/refresh")]
+        Task<ApiResponse<AuthSuccessResponse>> RefreshAsync([Body] RefreshTokenRequest refreshTokenRequest);
+    }
+}
---
+++ Tweetbook.Sdk/ITweetbookApi.cs
@@ -0,0 +1,28 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1.Request;
+using TweetBook.Contracts.V1.Response;
+
+namespace Tweetbook.Sdk
+{
+    [Headers("Authorization: Bearer")]
+    public interface ITweetbookApi
+    {
+        [Get("/api/v1/posts")]
+        Task<ApiResponse<IEnumerable<PostResponse>>> GetPostsAsync();
+
+        [Get("/api/v1/posts/{postId}")]
+        Task<ApiResponse<PostResponse>> GetAsync(Guid postId);
+
+        [Put("/api/v1/posts/{postId}")]
+        Task<ApiResponse<PostResponse>> UpdateAsync(Guid postId, [Body] UpdatePost postToBeUpdated);
+
+        [Post("/api/v1/posts")]
+        Task<ApiResponse<CreatePostResponse>> CreateAsync([Body] CreatePostRequest createPost);
+
+        [Delete("/api/v1/posts/{postId}")]
+        Task<ApiResponse<string>> DeleteAsync(Guid postId);
+    }
+}
