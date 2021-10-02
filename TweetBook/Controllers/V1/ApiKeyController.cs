aa852aad63e61e6559261234b7bc9427126d64b5
SWAROOP Shivanna <swarooprooney@gmail.com>
2021-10-02 14:49:26 +0530

    Api Key based authentication

---
+++ TweetBook/Controllers/V1/ApiKeyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TweetBook.Filters;
+
+namespace TweetBook.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class ApiKeyController : Controller
+    {
+        [HttpGet("Get")]
+        public IActionResult Get()
+        {
+            return Ok("You can have my secret");
+        }
+    }
+}
